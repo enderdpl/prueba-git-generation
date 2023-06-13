168d89c870ed2b359b35c6947bc6041b01502a90
ender <ender.1895.perez@gmail.com>
2023-06-13 09:35:47 -0500

    ejercicio profe

--- prueba.js
+++ prueba.js
@@ -1,5 +1,28 @@
-console.log("hola perro dog")
-console.log("hola gato")
-console.log("hola tortuga")
-console.log("hola dino")
-console.log("hola gato")
+// agregar 2 nuevos sabores de helados al array
+
+
+let iceCreamFlavors = [
+    "Chocolate",
+    "Strawberry",
+    "Vanilla",
+    "Pistacho",
+    "Neapolitan",
+    "Mint Chip",
+  ];
+  
+  
+  let iWantAIceCream = window.prompt("Que sabor de helado deseas?", "");
+  let iceCreamFound;
+  for (let i = 0; i < iceCreamFlavors.length; i++) {
+    if (iceCreamFlavors[i].toLowerCase() === iWantAIceCream.toLowerCase()) {
+      iceCreamFound = iceCreamFlavors[i];
+    }
+  }
+  if(iWantAIceCream === "") {
+     console.log("No elegiste un sabor de helado");
+  } else if (iceCreamFound) {
+    console.log(`Aquí está tu helado de ${iceCreamFound}, disfrútalo!`);
+  } else {
+    console.log(`Lo siento, no tenemos helados sabor ${iWantAIceCream}`);
+  }
+  
