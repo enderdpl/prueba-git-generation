fa8447add060ef5dc6363b1fa894fe6e2496d440
ender <ender.1895.perez@gmail.com>
2023-06-15 14:14:12 -0500

    ejercicios

--- prueba.js
+++ prueba.js
@@ -1,40 +1,88 @@
-// agregar 2 nuevos sabores de helados al array
 
 
-let iceCreamFlavors = [
-    "Chocolate",
-    "Strawberry",
-    "Vanilla",
-    "Pistacho",
-    "Neapolitan",
-    "Mint Chip",
-    "maracuya",
-    "mango"
-  ];
-  
-  
+//  --------> Ejercicio 1 <--------------------
+// alert("Hola mundo")
+//  --------> Ejercicio 2 <--------------------
+
+// document.write("Hello World");
+
+//  --------> Ejercicio 3 <--------------------
+
+// alert("el resultado de 5+3 = "+(5+3))
+
+//  --------> Ejercicio 4 <--------------------
 
 
-  let iWantAIceCream = window.prompt("Que sabor de helado deseas?", "");
-  let iceCreamFound;
+// let nombre= prompt("escriba su nombre")
+// document.write("     hola  ", nombre)
 
 
-
-  for (let i = 0; i < iceCreamFlavors.length; i++) {
-    if (iceCreamFlavors[i].toLowerCase() === iWantAIceCream.toLowerCase()) {
-      iceCreamFound = iceCreamFlavors[i];
-    }
-  }
+//  --------> Ejercicio 5 <--------------------
 
 
 
-  if(iWantAIceCream === "") {
-     console.log("No elegiste un sabor de helado");
+// let num1=parseInt( prompt("escriba num1"))
+// let num2= parseInt(prompt("escriba num2"))
+// console.log("la suma de ambos numero es= "(num1+num2))  
 
-  } else if (iceCreamFound) {
-    console.log(`Aquí está tu helado de ${iceCreamFound}, disfrútalo!`);
-    
-  } else {
-    console.log(`Lo siento, no tenemos helados sabor ${iWantAIceCream}`);
-  }
-  
+//  --------> Ejercicio 6 <--------------------
+// const numero1 = parseFloat(prompt("Ingrese el primer número:"));
+// const numero2 = parseFloat(prompt("Ingrese el segundo número:"));
+
+// if (numero1 > numero2) {
+//   console.log("El primer número es mayor: " + numero1);
+// } else if (numero2 > numero1) {
+//   console.log("El segundo número es mayor: " + numero2);
+// } else {
+//   console.log("Ambos números son iguales.");
+// }
+
+
+
+//  --------> Ejercicio 7 <--------------------
+
+
+
+// let number1 = parseInt(prompt("Ingrese el primer número:"));
+// let number2 = parseInt(prompt("Ingrese el segundo número:"));
+// let number3 = parseInt(prompt("Ingrese el tercer número:"));
+
+// let mayor = number1;
+
+// if (number2 > mayor) {
+//   mayor = number2;
+// }
+// if (number3 > mayor) {
+//   mayor = number3;
+// }
+// console.log("El número mayor es: " + mayor);
+
+
+
+
+//  --------> Ejercicio 8 <--------------------
+
+
+
+// let numerodiv = parseInt(prompt("Ingrese un número:"));
+
+// if (numerodiv % 2 === 0) {
+//   console.log("El número es divisible por 2.");
+// } else {
+//   console.log("El número no es divisible por 2.");
+// }
+
+
+//  --------> Ejercicio 9 <--------------------
+
+
+// const frase = prompt("Ingrese una frase:");
+// let conteo = 0;
+
+// for (let i = 0; i < frase.length; i++) {
+//   if (frase.charAt(i) === 'a') {
+//     conteo++;
+//   }
+// }
+
+// console.log("La letra 'a' aparece " + conteo + " veces en la frase.");
