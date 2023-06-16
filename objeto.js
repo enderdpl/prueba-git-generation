f58c826626f47dfda089c2c427b5217febb23673
ender <ender.1895.perez@gmail.com>
2023-06-16 14:46:14 -0500

    los 7 ejercicio de arreglo

--- objeto.js
+++ objeto.js
@@ -29,6 +29,7 @@ const alumno = {
   
   console.log("Nombre del alumno: " + nombreAlumno);
   console.log("Promedio de calificaciones: " + promedio);
+
 //   --------------------------------------ejercicio 3---------------------------------
 
 const objeto = {
@@ -41,5 +42,40 @@ const objeto = {
   };
   
   console.log("Precio neto: " + precioNeto() );
+
+  //   --------------------------------------ejercicio 4---------------------------------
+  const numeros = [];
+
+for (let i = 1; i <= 10; i++) {
+  numeros.push(i);
+}
+
+console.log(numeros);
+
+  //   --------------------------------------ejercicio 5---------------------------------
+  let amigos = ["pedro", "maria", "joan", "pili"];
+  console.log("Mis amigos son " + amigos[1] + " y " + amigos[2] + ".");
   
+
+  //   --------------------------------------ejercicio 6---------------------------------
+
+  function obtenerNombreMes(numero) {
+    const meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+    console.log("el mes es : ", meses[numero]) 
+  }
+  obtenerNombreMes(4)
+  //   --------------------------------------ejercicio 7---------------------------------
+  function sumaLista(lista) {
+    let suma = 0;
+    for (let i = 0; i < lista.length; i++) {
+      suma += lista[i];
+    }
+     console.log("El resultado de la suma es: " + suma);
+  }
   
+  sumaLista([2, 4, 5, 1, 2]);
+ 
+  
+ 
+  
+
