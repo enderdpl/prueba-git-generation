b9d2daa864e8304117a5374d64258f703ac91862
ender <ender.1895.perez@gmail.com>
2023-06-27 15:05:23 -0500

    ejercicio dom martes

--- doom.js
+++
@@ -1,8 +0,0 @@
-const nombre = document.getElementById("nombre");
-const apellido = document.getElementById("apellido");
-const p = document.getElementById("saludo");
-
-function enviarSaludo() {
-    console.log(nombre.value);
-    p.innerHTML = `Hola ${nombre.value} ${apellido.value}, bienvenido.`;
-}
